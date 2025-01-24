f613a6a4c80df022a156acaa135cfd0b5a595f6f
Chatinun <Chatinun.hobby@gmail.com>
2025-01-24 15:36:37 +0900

    test protect branch

--- src/ResultPage.tsx
+++ src/ResultPage.tsx
@@ -3,7 +3,7 @@ const ResultPage = ({ result }: { result: ResultType }) => {
   result.sort((a, b) => b.wins - a.wins);
   return (
     <>
-      <h1>Game Ended</h1>
+      <h1>Game Ended test</h1>
 
       <ul>
         {result.map((element, i) => {
