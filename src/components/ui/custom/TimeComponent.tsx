b886366fc30c48c8b869da3d0a247ecebc6ff089
Chatinun <Chatinun.hobby@gmail.com>
2025-02-03 10:11:45 +0900

    change axios

--- src/components/ui/custom/TimeComponent.tsx
+++ src/components/ui/custom/TimeComponent.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const TimeComponent = () => {
   const fetcher = async (url: string) => {
-    return await axios.get(url);
+    return (await axios.get(url)).data;
   };
 
   const { data, error, isLoading } = useSwr(
@@ -13,7 +13,7 @@ const TimeComponent = () => {
 
   if (error) return <>{error}</>;
   if (isLoading) return <>Is loading ...</>;
-  return <>{data?.data.time}</>;
+  return <>{data.time}</>;
 };
 
 export default TimeComponent;
